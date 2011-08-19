da110038e0f362f80602604432667a51d77c94c0
instcode <instcode@80ff207f-5179-d399-db5f-d6c34936ffd5>
2011-08-19 02:50:01 +0000

    Some new actions...

--- SanguoVn/lib/node-sanguo/lib/sanguo.js
+++ SanguoVn/lib/node-sanguo/lib/sanguo.js
@@ -139,6 +139,14 @@ Sanguo.prototype._parseChunk = function(chunk, callback) {
     this.buffer = messages[messages.length - 1];
 }
 
+Sanguo.prototype.openChest = function(type, callback) {
+    var command = this.create(Message.OPEN_BOX, [type])
+    this.flood(command, 1000, function(json) {
+        //var award = json.m.award;
+        callback(json.m);
+    });
+}
+
 Sanguo.prototype.enumItemsInStore = function(callback) {
     var command = this.create(Message.STOREHOUSE, [0, 15])
     this.send(command, function(json) {
@@ -161,6 +169,11 @@ Sanguo.prototype.enumEquipments = function(callback) {
 
 Sanguo.prototype.recruit = function(generalId, callback) {
     var command = this.create(Message.GENERAL_RECRUIT, [generalId]);
+    this.send(command, callback);
+}
+
+Sanguo.prototype.attackNPC = function(npc, callback) {
+    var command = this.create(Message.BATTLE_ARMY, [npc, '0']);
     this.send(command, callback);
 }
 
@@ -201,11 +214,6 @@ Sanguo.prototype.buy = function(goods, callback) {
 
 Sanguo.prototype.trade = function(type, useGold, callback) {
     var command = this.create(Message.MARKET_TRADE, [type, useGold]);
-    this.send(command, callback);
-}
-
-Sanguo.prototype.autoTrade = function(type, callback) {
-    var command = this.create(Message.MARKET_TRADE, [type, 0]);
     this.send(command, callback);
 }
 
@@ -272,4 +280,114 @@ Sanguo.prototype.enumGenerals = function(callback) {
     });
 }
 
+/**
+ * Dong Trac: 900001
+ * Truong Lo: 900004
+ * 
+ * #1:#2;#3
+ *
+ * #1: Unknown ('4' so far)
+ * #2: Level ('0')
+ * #3: Type (1: Free, 2: Ally, 3: Guid)
+ */
+Sanguo.prototype.createParty = function(map, options, callback) {
+    var params = '5' + ':' + options.level + ';' + options.type;
+    var command = this.create(Message.B_CREATE_TEAM, [map, params]);
+    this.send(command, function(json) {
+        callback(json);
+    });
+}
+
+Sanguo.prototype.enumParties = function(map, callback) {
+    var command = this.create(Message.B_GET_TEAM_INFO, [map]);
+    this.send(command, function(json) {
+        var teams = json.m.team;
+        for (var key in teams) {
+            callback(teams[key]);
+        }
+    });
+    
+}
+
+Sanguo.prototype.joinParty = function(teamId, callback) {
+    var command = this.create(Message.B_JOIN_TEAM, [teamId]);
+    this.send(command, function(json) {
+        callback(json);
+    });
+}
+
+Sanguo.prototype.getRefreshInfo = function(generalid, callback) {
+    var command = this.create(Message.GENERAL_GET_REFRESH_INFO, [generalid]);
+    this.send(command, function(json) {
+        console.log(JSON.stringify(json));
+        callback(json.m.general.originalattr);
+    });
+}
+
+Sanguo.prototype.improve = function(generalid, acceptance, callback) {
+    var command = this.create(Message.GENERAL_REFRESH, [generalid, 0]);
+    var self = this;
+    this.send(command, function(json) {
+        var confirm = '0';
+        if (acceptance(json.m.general)) {
+            confirm = '1';
+        }
+        var command = self.create(Message.GENERAL_REFRESH_CONFIRM, [generalid, confirm]);
+        self.send(command, callback);
+    });
+}
+
+Sanguo.prototype.conquer = function(playerid, callback) {
+    var command = this.create(Message.AREA_CONQUER_PLAYER, [playerid]);
+    this.send(command, callback);
+}
+
+
+Sanguo.prototype.sendCaravan = function(playerid, callback) {
+    var command = this.create(Message.CARAVAN_SEND, [playerid]);
+    this.send(command, callback);
+}
+
+Sanguo.prototype.invest = function(callback) {
+    var command = this.create(Message.WORLD_INVEST_AREA, ['9', '1']);
+    this.send(command, callback);
+}
+
+
+Sanguo.prototype.addConstructor = function(callback) {
+    var command = this.create(Message.ADD_C);
+    this.send(command, callback);
+}
+
+Sanguo.prototype.buyTrainPos = function(callback) {
+    var command = this.create(Message.GENERAL_BUY_TRAIN_POSITION);
+    this.send(command, callback);
+}
+
+/**
+ *  1 - Khan Vang
+ *  2 - Dong Trac
+ *  3 - Cong Ton Toan
+ *  4 - Tieu The Luc
+ *  5 - Truong Lo
+ *  6 - Vien Thuat
+ *  7 - Nghiem Bach Ho
+ *  8 - Luu Bieu
+ *  9 - Luu Chuong
+ * 10 - Ma Dang
+ * 11 - Manh Hoach
+ * 12 - Vien Thuat
+ * 13 - Lu Bo
+*/
+Sanguo.prototype.getMapReward = function(index, callback) {
+    //this.send(Message.OPEN_POWERMAP);
+    //this.send(Message.BATTLE, [index]);
+    var command = this.create(Message.BATTLE_GET_REWARDS, [index]);
+    this.send(command, callback);
+}
+
+Sanguo.prototype.collectGift = function(giftcode, callback) {
+    var command = this.create(Message.RECEIVE_GIFT, [giftcode]);
+    this.send(command, callback);
+}
 
